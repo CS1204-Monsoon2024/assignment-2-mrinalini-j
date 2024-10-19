c1c0218649a68ec393424e5e26e55a421489fdb0
mrinalini-j <mrinalini.jindal_ug2023@ashoka.edu.in>
2024-10-19 13:04:55 +0530

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -70,23 +70,35 @@ public:
     }
 
     void insert(int key) {
-        if ((double)count / size >= loadFactorThreshold) {
-            resize();
-        }
-
-        int i = 0;
-        int index;
-        do {
-            index = quadraticProbe(key, i);
-            i++;
-        } while (table[index].key != -1 && !table[index].isDeleted && table[index].key != key);
-
-        if (table[index].key == -1 || table[index].isDeleted) {
-            table[index].key = key;
-            table[index].isDeleted = false;
-            count++;
-        }
+    if ((double)count / size >= loadFactorThreshold) {
+        resize();  // Resize the table if the load factor threshold is exceeded
     }
+
+    int i = 0;
+    int index;
+    int firstDeletedIndex = -1;  // Track the first deleted index found
+    do {
+        index = quadraticProbe(key, i);
+        i++;
+
+        // Track the first deleted index to use later if necessary
+        if (table[index].isDeleted && firstDeletedIndex == -1) {
+            firstDeletedIndex = index;
+        }
+    } while (table[index].key != -1 && !table[index].isDeleted && table[index].key != key);
+
+    // If we find an empty slot, or a previously deleted slot, insert the key
+    if (table[index].key == -1 || table[index].isDeleted) {
+        // Prefer inserting into the first deleted index, if available
+        if (firstDeletedIndex != -1) {
+            index = firstDeletedIndex;
+        }
+        table[index].key = key;
+        table[index].isDeleted = false;
+        count++;
+    }
+}
+
 
     int search(int key) {
         int i = 0;
