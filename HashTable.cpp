35557f2613aead470290c97847acf4ed6a4fd52b
mrinalini-j <mrinalini.jindal_ug2023@ashoka.edu.in>
2024-10-19 12:29:42 +0530

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -2,19 +2,23 @@
 #include <vector>
 #include <cmath>
 
+struct HashEntry {
+    int key;
+    bool isDeleted;
+
+    HashEntry() : key(-1), isDeleted(false) {}
+};
+
 class HashTable {
-private:
-    struct HashEntry {
-        int key;
-        bool isDeleted;
-
-        HashEntry() : key(-1), isDeleted(false) {}
-    };
-
     std::vector<HashEntry> table;
     int size;
     int count;
-    double loadFactorThreshold = 0.8;
+    double loadFactorThreshold;
+
+public:
+    HashTable(int initialSize = 7, double loadFactor = 0.5) : size(initialSize), count(0), loadFactorThreshold(loadFactor) {
+        table.resize(size);
+    }
 
     int hash(int key) {
         return key % size;
@@ -24,21 +28,21 @@ private:
         return (hash(key) + i * i) % size;
     }
 
-    bool isPrime(int num) {
-        if (num <= 1) return false;
-        if (num == 2) return true;
-        if (num % 2 == 0) return false;
-        for (int i = 3; i * i <= num; i += 2) {
-            if (num % i == 0) return false;
-        }
+    bool isPrime(int n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (int i = 5; i * i <= n; i += 6)
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
         return true;
     }
 
-    int nextPrime(int num) {
-        while (!isPrime(num)) {
-            num++;
+    int nextPrime(int n) {
+        while (!isPrime(n)) {
+            n++;
         }
-        return num;
+        return n;
     }
 
     void resize() {
@@ -54,22 +58,17 @@ private:
 
                 // Use quadratic probing for inserting into newTable
                 do {
-                    newIndex = quadraticProbe(newKey, j) % newSize;  // Adjusted to use newSize
+                    newIndex = (hash(newKey) + j * j) % newSize;  // Adjusted to use newSize
                     j++;
-                } while (newTable[newIndex].key != -1 && !newTable[newIndex].isDeleted); // Stop if we find an empty or deleted slot
+                } while (newTable[newIndex].key != -1); // Stop if we find an empty slot
 
                 newTable[newIndex].key = newKey;
                 newTable[newIndex].isDeleted = false;
             }
         }
 
-        table = std::move(newTable);
+        table = newTable;
         size = newSize;
-    }
-
-public:
-    HashTable(int initialSize) : size(nextPrime(initialSize)), count(0) {
-        table.resize(size);
     }
 
     void insert(int key) {
@@ -82,6 +81,12 @@ public:
         do {
             index = quadraticProbe(key, i);
             i++;
+
+            // If the key already exists, return without inserting
+            if (table[index].key == key && !table[index].isDeleted) {
+                return; // Key already exists
+            }
+
             // If the slot is empty or deleted, we can insert
             if (table[index].key == -1 || table[index].isDeleted) {
                 table[index].key = key;
@@ -98,26 +103,19 @@ public:
         do {
             index = quadraticProbe(key, i);
             i++;
+            // Stop if we reach an empty slot that was never occupied
             if (table[index].key == -1 && !table[index].isDeleted) {
                 return -1;  // Key not found
             }
+            // Return the index if we find the key
         } while (table[index].key != key && i < size);
 
         return (table[index].key == key) ? index : -1;  // Return index if found, else -1
     }
 
     void remove(int key) {
-        int i = 0;
-        int index;
-        do {
-            index = quadraticProbe(key, i);
-            i++;
-            if (table[index].key == -1 && !table[index].isDeleted) {
-                return;  // Key not found, nothing to remove
-            }
-        } while (table[index].key != key && i < size);
-
-        if (table[index].key == key) {
+        int index = search(key);
+        if (index != -1) {
             table[index].isDeleted = true;
             count--;
         }
