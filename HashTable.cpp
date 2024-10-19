33984edbb9b0ff44cfdfab71251079a512fb203c
mrinalini-j <mrinalini.jindal_ug2023@ashoka.edu.in>
2024-10-19 13:20:02 +0530

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -42,66 +42,63 @@ private:
     }
 
     void resize() {
-    int newSize = nextPrime(2 * size);
-    std::vector<HashEntry> newTable(newSize);
+        int newSize = nextPrime(2 * size);
+        std::vector<HashEntry> newTable(newSize);
 
-    // Rehash all current keys into the new table
-    for (int i = 0; i < size; i++) {
-        if (table[i].key != -1 && !table[i].isDeleted) {
-            int newKey = table[i].key;
-            int j = 0;
-            int newIndex;
-            do {
-                newIndex = (newKey % newSize + j * j) % newSize;  // Use newSize here
-                j++;
-            } while (newTable[newIndex].key != -1);
+        // Rehash all current keys into the new table
+        for (int i = 0; i < size; i++) {
+            if (table[i].key != -1 && !table[i].isDeleted) {
+                int newKey = table[i].key;
+                int j = 0;
+                int newIndex;
+                do {
+                    newIndex = (newKey % newSize + j * j) % newSize;  // Use newSize here
+                    j++;
+                } while (newTable[newIndex].key != -1);
 
-            newTable[newIndex].key = newKey;
-            newTable[newIndex].isDeleted = false;
+                newTable[newIndex].key = newKey;
+                newTable[newIndex].isDeleted = false;
+            }
         }
+
+        table = newTable;
+        size = newSize;
     }
 
-    table = newTable;
-    size = newSize;
-}
-
-    table = newTable;
-    size = newSize;
-}
 public:
     HashTable(int initialSize) : size(nextPrime(initialSize)), count(0) {
         table.resize(size);
     }
 
-void insert(int key) {
-    if ((double)count / size >= loadFactorThreshold) {
-        resize();  // Resize the table if the load factor threshold is exceeded
-    }
-
-    int i = 0;
-    int index;
-    int firstDeletedIndex = -1;  // Track the first deleted index found
-    do {
-        index = quadraticProbe(key, i);
-        i++;
-
-        // Track the first deleted index to use later if necessary
-        if (table[index].isDeleted && firstDeletedIndex == -1) {
-            firstDeletedIndex = index;
+    void insert(int key) {
+        if ((double)count / size >= loadFactorThreshold) {
+            resize();  // Resize the table if the load factor threshold is exceeded
         }
-    } while (table[index].key != -1 && !table[index].isDeleted && table[index].key != key);
 
-    // If we find an empty slot, or a previously deleted slot, insert the key
-    if (table[index].key == -1 || table[index].isDeleted) {
-        // Prefer inserting into the first deleted index, if available
-        if (firstDeletedIndex != -1) {
-            index = firstDeletedIndex;
+        int i = 0;
+        int index;
+        int firstDeletedIndex = -1;  // Track the first deleted index found
+        do {
+            index = quadraticProbe(key, i);
+            i++;
+
+            // Track the first deleted index to use later if necessary
+            if (table[index].isDeleted && firstDeletedIndex == -1) {
+                firstDeletedIndex = index;
+            }
+        } while (table[index].key != -1 && !table[index].isDeleted && table[index].key != key);
+
+        // If we find an empty slot, or a previously deleted slot, insert the key
+        if (table[index].key == -1 || table[index].isDeleted) {
+            // Prefer inserting into the first deleted index, if available
+            if (firstDeletedIndex != -1) {
+                index = firstDeletedIndex;
+            }
+            table[index].key = key;
+            table[index].isDeleted = false;
+            count++;
         }
-        table[index].key = key;
-        table[index].isDeleted = false;
-        count++;
     }
-}
 
     int search(int key) {
         int i = 0;
@@ -145,3 +142,4 @@ void insert(int key) {
         std::cout << std::endl;
     }
 };
+
